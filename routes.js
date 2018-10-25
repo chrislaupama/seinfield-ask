4658ea6bfc9003c8da3f869fe21bc58136c2f9b3
Chris Laupama <chrislaupama@icloud.com>
2018-10-26 12:24:48 +1300

    got the feedback working

--- routes.js
+++ routes.js
@@ -28,18 +28,10 @@ router.get('/ask', (req, res) => {
   res.render('./students/ask', data)
 })
 
-// router.get('/ask/:id', (req, res) => {
-//   const id = parseInt(req.params.id, 10)
-//   let array = Array.from(data.students)
-//   let student = array.find(function (students) {
-//     return id === students.id
-//   })
-//   res.render('./students/ask', data)
-// })
 
 // ASK SUBMIT FORM
 router.post('/ask', (req, res) => {
-  let inputId = req.body.id
+  let inputId = Number(req.body.id)
   let inputFeedback = req.body.feedback
   const student = data.students.find(student => student.id === inputId)
   data.students[inputId - 1] = student
@@ -48,18 +40,6 @@ router.post('/ask', (req, res) => {
     // eslint-disable-next-line no-console
     if (err) { console.error(err) }
   })
-  res.redirect('/students/profile/' + inputId)
+  res.redirect('/students/' + inputId)
 })
 
-// router.post('/ask/:id', (req, res) => {
-//   const id = parseInt(req.params.id, 10)
-//   let array = Array.from(data.students)
-//   let student = array.find(arr => arr.id === id)
-//   student.name = req.body.name
-//   student.feedback = req.body.feedback
-//   fs.writeFile('./data.json', JSON.stringify(data), 'utf-8', (err) => {
-//     // eslint-disable-next-line no-console
-//     if (err) { console.error(err) }
-//   })
-//   res.redirect('/ask/:id' + id)
-// })
