f046eba847371a199478d4ed6c5aaf51e7d9d92b
Chris Laupama <chrislaupama@icloud.com>
2018-10-26 15:59:01 +1300

    stable version

--- routes.js
+++ routes.js
@@ -28,18 +28,18 @@ router.get('/ask', (req, res) => {
   res.render('./students/ask', data)
 })
 
-
 // ASK SUBMIT FORM
 router.post('/ask', (req, res) => {
   let inputId = Number(req.body.id)
   let inputFeedback = req.body.feedback
+  let sender = req.body.sender
   const student = data.students.find(student => student.id === inputId)
   data.students[inputId - 1] = student
   student.feedback = inputFeedback
+  student.sender = sender
   fs.writeFile('./data.json', JSON.stringify(data), (err) => {
     // eslint-disable-next-line no-console
     if (err) { console.error(err) }
   })
   res.redirect('/students/' + inputId)
 })
-
